f66e967c0322bb791fda03b9414e3fdce9f116dd
Pablo Speroni <pablolsperoni@gmail.com>
2020-06-29 16:11:54 -0300

    Improve the taxis controller

---
+++ Taxi.web/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Taxi.web.Data.Entities;
+
+namespace Taxi.web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { 
+
+        }
+        public DbSet<TaxiEntity> Taxis { get; set; }
+    }
+}
+
